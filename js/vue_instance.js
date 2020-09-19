ab046a50a825a015dcc0bd5833f1fe43928ab333
Lucas Marra <marralucas1@gmail.com>
2020-09-19 19:30:34 -0300

    update v2 - Modelos

--- js/vue_instance.js
+++ js/vue_instance.js
@@ -1,28 +1,29 @@
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            models: [],
-            loading: true,
-            errored: false,           
-        }
-    },
-    mounted() {
-        this.getModels();
-    },
-    
-    methods: {
-        getModels() {
-            axios
-                .get('')
-                .then((response) => {
-                    
-                })
-                .catch(error => {
-                    
-                })
-                .finally(() => this.loading = false)
-        },
-    }
+  el: "#app",
+  data() {
+    return {
+      models: [],
+      loading: true,
+      errored: false,
+    };
+  },
+  mounted() {
+    this.getModels();
+  },
 
-})
+  methods: {
+    getModels() {
+      axios
+        .get("https://parallelum.com.br/fipe/api/v1/carros/marcas")
+        .then((response) => {
+          this.models = response.data;
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
+  },
+});
